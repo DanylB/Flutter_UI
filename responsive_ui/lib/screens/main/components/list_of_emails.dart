17e9dc77e8edfcc72da81de7dd2a8e8fcdf6fc5c
DanylB <bbogomedov5@gmail.com>
2021-02-27 18:19:17 +0300

    add responsive_ui

---
+++ responsive_ui/lib/screens/main/components/list_of_emails.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:outlook/components/side_menu.dart';
+import 'package:outlook/models/Email.dart';
+import 'package:outlook/screens/email/email_screen.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+import '../../../constants.dart';
+import '../../../responsive.dart';
+import 'email_card.dart';
+
+class ListOfEmails extends StatefulWidget {
+  const ListOfEmails({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _ListOfEmailsState createState() => _ListOfEmailsState();
+}
+
+class _ListOfEmailsState extends State<ListOfEmails> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 250),
+        child: SideMenu(),
+      ),
+      body: Container(
+        // padding: EdgeInsets.all(kDefaultPadding),
+        color: kBgDarkColor,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Row(
+                  children: [
+                    if (!Responsive.isDesktop(context))
+                      IconButton(
+                        icon: Icon(Icons.menu),
+                        onPressed: () {
+                          _scaffoldKey.currentState.openDrawer();
+                        },
+                      ),
+                    if (!Responsive.isDesktop(context)) SizedBox(width: 5),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          fillColor: kBgLightColor,
+                          filled: true,
+                          suffixIcon: Padding(
+                            padding:
+                                const EdgeInsets.all(kDefaultPadding * 0.75),
+                            child: WebsafeSvg.asset(
+                              "assets/Icons/Search.svg",
+                              width: 24,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: kDefaultPadding),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Row(
+                  children: [
+                    WebsafeSvg.asset(
+                      "assets/Icons/Angle down.svg",
+                      width: 16,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "Sort by date",
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Spacer(),
+                    MaterialButton(
+                      minWidth: 20,
+                      onPressed: () {},
+                      child: WebsafeSvg.asset(
+                        "assets/Icons/Sort.svg",
+                        width: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: kDefaultPadding),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: emails.length,
+                  itemBuilder: (context, index) => EmailCard(
+                    isActive: Responsive.isMobile(context) ? false : index == 0,
+                    email: emails[index],
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EmailScreen(
+                            email: emails[index],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
