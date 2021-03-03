fe0d408cd58f9d8c9159b93d5a182e49a9449c0e
DanylB <bbogomedov5@gmail.com>
2021-03-03 21:57:52 +0300

    add HomePage online_learning_ui

--- online_learning_ui/lib/main.dart
+++ online_learning_ui/lib/main.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:online_learning_ui/screens/export_screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
